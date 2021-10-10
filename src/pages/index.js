3f981bf0542c39b2b1c1ddc27be7b220a6382194
jeet2610 <jeetshah0@icloud.com>
2021-10-11 01:25:24 +0530

    Changed by Jeet

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,39 +3,39 @@ import * as React from "react"
 <link rel="manifest" href="manifest.webmanifest"></link>
 const IndexPage = () => {
   return (
-    <div className="h-80 w-60 bg-gray-200 flex flex-col justify-between ">
+    <div class="h-80 w-60 bg-gray-200 flex flex-col justify-between ">
 
-<div className="h-10 w-full bg-gray-900 flex justify-between items-center pl-4 pr-4">
-  <div className="h-3 w-2/5  bg-gray-200">
+    <div class="h-10 w-full bg-gray-900 flex justify-between items-center pl-4 pr-4">
+      <div class="h-3 w-2/5  bg-gray-200">
+        
+      </div>
     
-  </div>
-
-  <div className="h-3 w-3 bg-gray-200"></div>
-</div>
-
-
-<div className="w-full  h-3/6 flex justify-center items-center mt-5">
-<div className="h-full w-full flex flex-col justify-between items-center ">
- <div className="bg-red-600 w-2/5 h-2/3">
-
- </div>
-  
-  <h1 className="text-center text-sm">book your pandit now  <br /> with indiaohyes </h1>
-
-  <h2 className="text-sm">Service</h2>
-  </div>
-
-</div>
-
-<div className="h-1/4 bg-yellow-50 flex justify-center items-center pr-5 pl-5">
-   <div className="grid grid-cols-3 h-full w-full gap-3 py-4 ">
-     <div className="bg-yellow-900"> </div>
-        <div className="bg-yellow-300"> </div>
-           <div className="bg-yellow-300"> </div>
-   </div>
-</div>
-
-</div>
+      <div class="h-3 w-3 bg-gray-200"></div>
+    </div>
+    
+    
+    <div class="w-full  h-3/6 flex justify-center items-center mt-5">
+    <div class="h-full w-full flex flex-col justify-between items-center ">
+     <div class="bg-red-600 w-2/5 h-2/3">
+    
+     </div>
+      
+      <h1 class="text-center text-sm">book your pandit now  <br /> with indiaohyes </h1>
+    
+      <h2 class="text-sm">Service</h2>
+      </div>
+    
+    </div>
+    
+    <div class="h-1/4 bg-yellow-50 flex justify-center items-center pr-5 pl-5">
+       <div class="grid grid-cols-3 h-full w-full gap-3 py-4 ">
+         <div class="bg-yellow-900"> </div>
+            <div class="bg-yellow-300"> </div>
+               <div class="bg-yellow-300"> </div>
+       </div>
+    </div>
+    
+    </div>
   
   )
 }
