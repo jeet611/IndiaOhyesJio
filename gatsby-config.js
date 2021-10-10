0d1ee15c34bd6a946de8c7a84acccdc481f1bf40
jeet2610 <jeetshah0@icloud.com>
2021-10-11 02:18:21 +0530

    Added Kaisdk and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,10 +19,18 @@ module.exports = {
       start_url: "/",
       background_color: "#fffff",
       theme_color: "#fffff",
-      "cursor":true,
+      "fullscreen": true,
+      "type": "privileged",
+      "permissions": {
+    "spatialnavigation-app-manage": {
+     "navigator.spatialNavigationEnabled" : true
+    }
+    
+       },
+       "cursor": true,
       // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
       // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-      display: "standalone",
+      display: "browser",
       icon: "src/images/icon.png", // This path is relative to the root of the site.
       // An optional attribute which provides support for CORS check.
       // If you do not provide a crossOrigin option, it will skip CORS for manifest.
